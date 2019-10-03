04a98ad54a86ca0ab4a9b2abcbc1791c7f04dd38
Sky JIA <me@skyjia.com>
2019-10-03 15:28:27 +0800

    fix cid

--- rpc/cid/context.go
+++ rpc/cid/context.go
@@ -13,10 +13,21 @@ const (
 )
 
 func ContextWithCid(ctx context.Context, cid string) context.Context {
+
+	md, ok := metadata.FromContext(ctx)
+	if !ok {
+		md = make(metadata.Metadata)
+	} else {
+		md = metadata.Copy(md)
+	}
+
+	md[http.CanonicalHeaderKey(MetaCidKey)] = cid
+	return metadata.NewContext(ctx, md)
+
 	// go 底层源码里面对 Key 传递的时候做了 CanonicalMIMEHeaderKey 处理
-	return metadata.NewContext(ctx, map[string]string{
-		http.CanonicalHeaderKey(MetaCidKey): cid,
-	})
+	// return metadata.NewContext(ctx, map[string]string{
+	// 	http.CanonicalHeaderKey(MetaCidKey): cid,
+	// })
 }
 
 // CidFromContext 从 Context 中获取 cid 的值
