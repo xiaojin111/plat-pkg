e7f571aa9b55fc7de44cf79f8f62562dd7d292fc
Sky JIA <me@skyjia.com>
2019-10-03 15:50:08 +0800

    fix wrapper

---
+++ rpc/wrapper/formatmeta/format_metadata_wrapper.go
@@ -0,0 +1,28 @@
+package formatmeta
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/micro/go-micro/server"
+
+	"github.com/micro/go-micro/metadata"
+)
+
+// FormatMetadataWrapper is a handler wrapper that format all metadata keys as http.CanonicalHeaderKey.
+func FormatMetadataWrapper(fn server.HandlerFunc) server.HandlerFunc {
+
+	return func(ctx context.Context, req server.Request, rsp interface{}) error {
+
+		md, _ := metadata.FromContext(ctx)
+		nmd := metadata.Metadata{}
+		for k, v := range md {
+			nmd[http.CanonicalHeaderKey(k)] = v
+		}
+
+		ctx = metadata.NewContext(ctx, nmd)
+
+		err := fn(ctx, req, rsp)
+		return err
+	}
+}
