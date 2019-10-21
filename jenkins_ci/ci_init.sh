8ddc15620e606fd46c526b9d505775f74f267255
juemengxin <601224464@qq.com>
2019-10-21 17:51:13 +0800

    test

--- jenkins_ci/ci_init.sh
+++ jenkins_ci/ci_init.sh
@@ -1,7 +1,8 @@
 # 初始化项目
 go version
 set -e
-GO111MODULE=off go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
+# GO111MODULE=off go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
 CUR=`dirname $0`
 cd ${CUR}/..
-go  mod download
+# go  mod download
+make  setup
