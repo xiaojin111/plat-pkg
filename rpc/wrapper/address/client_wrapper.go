59fe779cc5e71832fed51b87e0514947d18c647b
Sky JIA <me@skyjia.com>
2019-11-18 10:11:25 +0800

    refactor

--- rpc/wrapper/address/client_wrapper.go
+++ rpc/wrapper/address/client_wrapper.go
@@ -6,12 +6,12 @@ import (
 	"github.com/micro/go-micro/client"
 )
 
-type AddressWrapper struct {
+type ClientAddressWrapper struct {
 	client.Client
 	address string
 }
 
-func (w *AddressWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
+func (w *ClientAddressWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
 	if len(w.address) > 0 {
 		o := []client.CallOption{}
 		if len(opts) > 0 {
@@ -24,8 +24,8 @@ func (w *AddressWrapper) Call(ctx context.Context, req client.Request, rsp inter
 
 	return w.Client.Call(ctx, req, rsp, opts...)
 }
-func NewAddressWrapper(address string) client.Wrapper {
+func NewClientAddressWrapper(address string) client.Wrapper {
 	return func(c client.Client) client.Client {
-		return &AddressWrapper{c, address}
+		return &ClientAddressWrapper{c, address}
 	}
 }
