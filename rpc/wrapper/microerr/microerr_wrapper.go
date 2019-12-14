82ec22d1bd5fcf58656fc56905dd0e3e9e6272ad
Sky JIA <me@skyjia.com>
2019-12-14 19:11:18 +0800

    refactor microerr wrapper

--- rpc/wrapper/microerr/microerr_wrapper.go
+++ rpc/wrapper/microerr/microerr_wrapper.go
@@ -13,40 +13,56 @@ import (
 	"github.com/jinmukeji/plat-pkg/rpc/errors"
 )
 
-const defaultSvcName = "com.jinmuhealth.platform"
+// 错误样式
+const (
+	// ErrStyleSimple 简易模式错误输出样式。通常用于面向最终用户输出，本样式隐藏内部错误信息。
+	ErrStyleSimple = "microsimple"
+	// ErrStyleDetailed 详细模式错误输出样式。通常用于面向开发者或系统运维人员输出，本样式输出内部错误信息。
+	ErrStyleDetailed = "microdetailed"
+	// ErrStyleDetailed Micro原始错误输出样式。
+	ErrStyleRaw = "raw"
+)
+
+const (
+	ErrorStyleMetaKey      = "x-err-style"       // Metadata key / HTTP Header
+	ApplicationErrorStatus = "Application Error" // 错误状态
+	ApplicationErrorCode   = 520                 // 错误状态码
+)
 
 func MicroErrWrapper(fn server.HandlerFunc) server.HandlerFunc {
 
 	return func(ctx context.Context, req server.Request, rsp interface{}) error {
-		svcName := defaultSvcName
+		var svcName string
 		if svc, ok := micro.FromContext(ctx); ok {
 			svcName = svc.Server().Options().Name
 		}
 
 		err := fn(ctx, req, rsp)
 		if err != nil {
-			style := strings.ToLower(errorStyleFromContext(ctx))
+			var rErr error
+			style := errorStyleFromContext(ctx)
 
 			switch style {
-			case "microsimple":
+			case ErrStyleSimple:
 				// 使用简化版信息输出，不输出内部错误信息
 				if e, ok := err.(*errors.RpcError); ok {
-					return wrapError(svcName, e.Error())
+					rErr = wrapError(svcName, e.Error())
 				}
 
-			case "microdetailed":
+			case ErrStyleDetailed:
 				// 使用详细版信息输出，输出内部错误信息
 				if e, ok := err.(*errors.RpcError); ok {
-					return wrapError(svcName, e.DetailedError())
+					rErr = wrapError(svcName, e.DetailedError())
 				}
-			case "raw":
+			case ErrStyleRaw:
 				// 不做处理，直接输出原始的
-				// return err
+				rErr = err
 			default:
-				// return err
+				// 其它未知的，直接输出原始的
+				rErr = err
 			}
 
-			return err
+			return rErr
 		}
 
 		return nil
@@ -56,23 +72,24 @@ func MicroErrWrapper(fn server.HandlerFunc) server.HandlerFunc {
 func wrapError(id, detail string) error {
 	err := &me.Error{
 		Id:     id,
-		Code:   520,
+		Code:   ApplicationErrorCode,
 		Detail: detail,
-		Status: "Application Error",
+		Status: ApplicationErrorStatus,
 	}
 
 	return err
 }
 
 func errorStyleFromContext(ctx context.Context) string {
-	style := "Raw" // default
+	style := ErrStyleRaw // default
 
 	if md, ok := metadata.FromContext(ctx); ok {
 		// available style:
 		//  - RpcError
 		//  - MicroSimple
-		//  - microdetailed
-		style = md[http.CanonicalHeaderKey("x-err-style")]
+		//  - MicroDetailed
+		style = md[http.CanonicalHeaderKey(ErrorStyleMetaKey)]
+		style = strings.ToLower(style)
 	}
 
 	return style
